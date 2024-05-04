03783aecab61b5b7b81c9610d48e6b7d8cf7bb09
ranimghodhbani <159330924+ranimghodhbani@users.noreply.github.com>
2024-05-04 20:30:04 +0100

    Create Voiture.java

---
+++ Tp2/Exercice2/Voiture.java
@@ -0,0 +1,31 @@
+package Tp2.Exercice2;
+
+
+import java.io.*;
+public class Voiture implements Serializable {
+private int carburant;
+private String model;
+private String type;
+private static int capacite = 300;
+Voiture(String _type, String _model) {
+type = _type;
+model = _model;
+carburant = 0;
+}
+public void setCarburant(int c) {
+int maxi = capacite - carburant;
+if (c < maxi) {
+carburant += c;
+System.out.println("Le remplissage a été effectué sans problème.");
+} else {
+carburant = capacite;
+System.out.println((c - maxi) + " litre(s) de carburant ont débordé.");
+}
+}
+public int getCarburant() {
+return carburant;
+}
+public int getCapacite() {
+return capacite;
+}
+}
